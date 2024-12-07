92536428737b0c55173b5a7d26b590961c44fb5a
Saqman77 <saqlainhaider77@gmail.com>
2024-12-07 08:44:12 +0500

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     // depending on your application, base can also be "/"
     root: 'src/',
+    publicDir: '../public/',
     base: './',
     plugins: [react()],
     esbuild: {
