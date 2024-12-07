53140739143d0675e758c4788dfad87b9bb07442
Saqman77 <saqlainhaider77@gmail.com>
2024-12-07 11:23:07 +0500

    b

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import HomePage from "@pages/homepage";
 import Imprint from "@pages/Imprint";
 import News from "@pages/news";
 import Works from "@pages/works";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/news" element={<News />} />
@@ -16,7 +16,7 @@ function App() {
           <Route path="/imprint" element={<Imprint />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
