1559b82e8c4fd4d14c25daa7ce329f28646a2e85
Saqman77 <saqlainhaider77@gmail.com>
2024-12-07 08:35:24 +0500

    build

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,10 @@ export default defineConfig({
         host:true,
         open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
     },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
 })
