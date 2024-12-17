263f2cae7f8117e86036610d5c3ddde97db93cc9
Saqman77 <saqlainhaider77@gmail.com>
2024-12-17 10:01:02 +0500

    changed projector

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import "@styles/footer.css"
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy;PROJECT23 CREATIVE STUDIO</p>
+      <p>&copy;PROJECTOR23 CREATIVE STUDIO</p>
       <p className="rightelem">LAST UPDATE : JULY 2024</p>
     </footer>
   );
--- src/components/homepage/Top.jsx
+++ src/components/homepage/Top.jsx
@@ -25,7 +25,7 @@ const Top = ({ videoRef }) => {
               />
             </div>
             <div className="main" >
-              <h1>PROJECT23</h1>
+              <h1>PROJECTOR23</h1>
               <h1>CREATIVE STUDIO</h1>
             </div>
     
